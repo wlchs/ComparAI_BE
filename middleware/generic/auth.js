311baa9ed330a76fff6442ce7425735ef1ccace8
Laszlo Benedek Borbely <laszlobenedek.borbely@ge.com>
2018-02-21 16:34:07 +0100

    Omit error on wrong JWT

--- middleware/generic/auth.js
+++ middleware/generic/auth.js
@@ -13,10 +13,14 @@ module.exports = objectRepository => {
     let authHeader = req.get('Authorization');
     if (authHeader && authHeader.split(' ')[0] == 'Bearer:') {
       let token = authHeader.split(' ')[1];
-      var decoded = jwt.verify(token, ENVIRONMENTS.auth_secret);
 
-      res.tpl.user_db_id = decoded._id;
-      return next();
+      try {
+        var decoded = jwt.verify(token, ENVIRONMENTS.auth_secret);
+
+        res.tpl.user_db_id = decoded._id;
+        return next();
+      }
+      catch(ignore) {}
     }
 
     return next("Failed to authenticate user!");
