74a85ab933fd660d0b8f02568a25b744ce5d1848
Laszlo Benedek Borbely <laszlobenedek.borbely@ge.com>
2018-03-28 10:08:23 +0200

    production changes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+var path = require('path');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var compression = require('compression');
+var helmet = require('helmet');
 
 /**
  * Parse parameters in POST
@@ -16,7 +19,17 @@ require('./routes/user')(app);
 require('./routes/image')(app);
 require('./routes/category')(app);
 
+app.use(helmet());
+app.use(compression()); //Compress all routes
 app.use(express.static('public'))
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
 
 var server = app.listen(3300, function () {
   console.log("Server listening on port 3300!");
